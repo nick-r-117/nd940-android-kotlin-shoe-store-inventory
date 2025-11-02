535d5d76dee8d7ab77915fcb9ac1667845199f76
Nicholas Rosas <nicholasirosas@gmail.com>
2025-11-01 17:04:45 -0700

    [Shoe Store Inventory][Navigation] Setup basic navigation and initial screen

---
+++ starter/app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
@@ -0,0 +1,27 @@
+package com.udacity.shoestore.screens.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.LoginFragmentBinding
+
+class LoginFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<LoginFragmentBinding>(
+            inflater,
+            R.layout.login_fragment,
+            container,
+            false
+        )
+
+        return binding.root
+    }
+}
