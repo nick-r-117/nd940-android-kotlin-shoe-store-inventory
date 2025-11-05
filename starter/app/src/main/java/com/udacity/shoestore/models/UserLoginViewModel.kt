1c37d04fb71ec8eda4b888ec0b331f704d979fc9
Nicholas Rosas <nicholasirosas@gmail.com>
2025-11-04 18:39:11 -0800

    [Show Store Inventory][Login] Create base UI layout for Login Screen

---
+++ starter/app/src/main/java/com/udacity/shoestore/models/UserLoginViewModel.kt
@@ -0,0 +1,53 @@
+package com.udacity.shoestore.models
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import timber.log.Timber
+
+class UserLoginViewModel : ViewModel() {
+
+    private var _email: String? = null
+    private var _password: String? = null
+
+    private val _loginSuccess = MutableLiveData<Boolean>()
+    val loginSuccess: LiveData<Boolean>
+        get() = _loginSuccess
+
+    init {
+        _loginSuccess.value = false
+    }
+
+    fun createAccount(email: String?, password: String?) {
+        if (validate(email, password)) {
+            _email = email
+            _password = password
+
+            _loginSuccess.value = true
+
+            // TODO: Write to DB
+        }
+    }
+
+    fun login(email: String?, password: String?) {
+        if (verifyCredentials(email, password)) {
+            _email = email
+            _password = password
+            _loginSuccess.value = true
+        }
+    }
+
+    private fun validate(email: String?, password: String?): Boolean {
+        if (email.isNullOrEmpty() || password.isNullOrEmpty()) {
+            return false
+        }
+
+        // TODO: Error handling
+        return true
+    }
+
+    private fun verifyCredentials(email: String?, password: String?): Boolean {
+        return validate(email, password)
+        // TODO: Read from DB
+    }
+}
--- starter/app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
@@ -6,10 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.LoginFragmentBinding
+import com.udacity.shoestore.models.UserLoginViewModel
+import timber.log.Timber
 
 class LoginFragment : Fragment() {
+
+    private lateinit var userLoginViewModel: UserLoginViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,6 +27,33 @@ class LoginFragment : Fragment() {
             container,
             false
         )
+
+        userLoginViewModel = ViewModelProvider(this).get(UserLoginViewModel::class.java)
+        userLoginViewModel.loginSuccess.observe(this.viewLifecycleOwner, { loginSuccess ->
+            if (loginSuccess) {
+                Timber.d("Login successful")
+            }
+        })
+
+        binding.userLoginViewModel = userLoginViewModel
+        binding.lifecycleOwner = this.viewLifecycleOwner
+
+        binding.loginButton.setOnClickListener { view: View ->
+            // Read email & password fields
+            val email = binding.emailEditText.text?.toString()
+            val password = binding.passwordEditText.text?.toString()
+
+            // TODO: Error handing -> Store user credentials -> Navigate to onboarding screen
+            userLoginViewModel.login(email, password)
+        }
+        binding.createAccountButton.setOnClickListener { view: View ->
+            // Read email & password fields
+            val email = binding.emailEditText.text?.toString()
+            val password = binding.passwordEditText.text?.toString()
+
+            // TODO: Error handing -> Store user credentials -> Navigate to onboarding screen
+            userLoginViewModel.createAccount(email, password)
+        }
 
         return binding.root
     }
